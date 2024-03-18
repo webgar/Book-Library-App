ed0093d76d7f8d174e778c171136bd6fc5ed9a5a
mzafar91@mail.ru <mzafar91@mail.ru>
2024-03-18 11:08:50 +0500

    Added errorSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './slices/booksSlice'
+import errorReducer from './slices/errorSlice'
 import filterReducer from './slices/filterSlice'
 
 const store = configureStore({
 	reducer: {
 		books: booksReducer,
 		filter: filterReducer,
+		error: errorReducer,
 	},
 })
 export default store
